540c40f835e467141cef0d335ca49069d4f054c4
Amyn617 <aarfaoui617@gmail.com>
2025-05-19 09:07:26 +0100

    first commit

---
+++ src/main/java/ma/enset/creditbancaire/CreditBancaireApplication.java
@@ -0,0 +1,13 @@
+package ma.enset.creditbancaire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditBancaireApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditBancaireApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ma/enset/creditbancaire/CreditBancaireApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.enset.creditbancaire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditBancaireApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
